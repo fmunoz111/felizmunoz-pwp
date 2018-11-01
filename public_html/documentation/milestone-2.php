dde49606e6408869353097893b8edffe978183c3
felizmunoz <fmunoz273@gmail.com>
2018-11-01 09:33:30 -0600

    commit and push of milestone2a

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Feliz Munoz's Milestone 2a</title>
+	</head>
+	<body>
+		<div>
+			<h1>Milestone 2a</h1>
+			<h3>Navigation Bar</h3>
+				<p>The navigation bar is where people will be able to find the parts of my webpage and when they click on them they will be directed to that part on my webpage.  There is About, Testimonials, Portfolio, and Contact.  There will also be music playing when you get to my webpage and to the right of the navigation bar, you will find that there is a music section where you can pause and play the music and control the volume.  This component is not 'set it stone' and if it is able to happen, I will have it on my webpage</p>
+		</div>
+		<div>
+			<h3>Into and About Me</h3>
+				<p>My name and what I do a main focus on the webpage.  I want people to see that first and then go from there on the page.  On the left side of the webpage, left of my name will be the section for my 'about me'.  My about me will give a brief background of who I am, where I'm from, how I got into web development, and what I want to do with web development.</p>
+		</div>
+		<div>
+			<h3>Portfolio</h3>
+				<p>My portfolio section is pretty straight forward.  I would like to put our capstone project here as a link for people to click on and it will open up in a new tab.  I want to be able to work on something on the side in my own time to showcase more of my work as a front end web developer.  This personal project will also open up into a new tab that way it is easier for the user to see what I have an go back to my pwp.</p>
+		</div>
+		<div>
+			<h3>Testimonials</h3>
+				<p>The testimonials will be words from my peers, career buddy, capstone group, and possibly instructors about me as a person, my work ethic, and whatever else (postive) they want to say about me.  This will allow for future employers to get a glipse of who I am all around from others than myself.  I want the testimonials to rotate one by one on that section of the page for say 30 seconds at a time.</p>
+		</div>
+		<div>
+			<h3>Contact Me</h3>
+				<p>The contact me section is straight forward, as well.  Here is where future employers or people who want to work/collaborate together will be able to leave me a message with their name and email for me to contact back.  Anyone will be able to leave me a message and way to contact them back.</p>
+		</div>
+		<div>
+			<h3>Desktop Wireframe</h3>
+			<img src="website-wireframe.png" alt="website wireframe" width="700" height="700">
+		</div>
+		<div>
+			<h3>Mobile Wireframe</h3>
+			<img src="mobile-wireframe1.png" alt="mobile wireframe" width="400" height="600">
+			<img src="mobile-wirefram2.png" alt="mobile wireframe" width="400" height="600">
+		</div>
+	</body>
+
+</html>
