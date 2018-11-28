6dede54c5a8fc450fba9f8765f213bbd2b53ce34
felizmunoz <fmunoz273@gmail.com>
2018-11-27 17:06:34 -0700

    changed to a local host

---
+++ public_html/index.php
@@ -0,0 +1,197 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<link rel="stylesheet" href="stylesheet.css">
+
+		<title>Feliz Munoz PWP</title>
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+		<!-- add jquery-validate-->
+		<script src="js/jquery-validate.js"></script>
+	</head>
+	<body>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+		<!-- add missing Jquery files -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+		<!-- add jquery-validate-->
+		<script src="js/jquery-validate.js"></script>
+
+<header>
+	<nav class="nav-custom">
+		<div class="container-fluid m-3" id="color">
+			<ul class="nav justify-content-center">
+				<li class="nav-item">
+					<a class="nav-link" href="#about">About</a></li>
+				<li class="nav-item">
+					<a class="nav-link" href="#testimonials">Testimonials</a></li>
+				<li class="nav-item">
+					<a class="nav-link" href="#portfolio">Portfolio</a></li>
+				<li class="nav-item">
+					<a class="nav-link" href="#contact">Contact Me</a></li>
+			</ul>
+		</div>
+	</nav>
+</header>
+
+<section>
+	<div class="jumbotron" id="intro">
+		<div class="parallax_section parallax_image_first">
+			<div class="center">
+				<h1 class="text-right"><strong>Feliz Munoz</strong></h1>
+				<h4 class="text-right">Web Developer</h4>
+			</div>
+		</div>
+		<!--Block with content-->
+		<div class="content_block">
+			<div class="center">
+				<div class="about" id="about">
+					<div class="container-fluid text-center">
+						<h2 class="about me">About</h2>
+						<p>Hello!  I am Feliz Munoz and I am a native New Mexican full stack web developer.  I have recently completed the Full Stack Web Development Bootcamp through CNM Ingenuity.  I have experince with html, css, javascript, mySQL, php, bootstrap, and node.js. I have a background in working for non-profits, event organization, teaching, and working in teams.  I am a driven individual and I adapt to any environment I am in.  I focus on goals, I enjoy routine, and I always like to have something going on in my life.  I enjoy helping others achieve their goals.</p>
+
+						<br>
+
+						<p>I am an active member of a non-profit known as the Organ Transplant Awareness Program where we aim to spread the awareness of organ donation in Albuquerque.  I began the Transplant Trek in 2011 and OTAP has since then become co-host of the walk where proceeds benefit the Jaime Munoz Scholarship and the Betty Montoya Scholarship.  I have a passion for bettering the community I am from and I want to use web development to continue my help to Albuquerque.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="parallax_section parallax_image_second">
+			<div class="center">
+				<div class="Testimonials" id="testimonials">
+					<div class="container-fluid text-center">
+						<h2 class="align-center">Testimonials</h2>
+						<br>
+						<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+							<div class="carousel-inner">
+								<div class="carousel-item active">
+									<p>
+										This is testimonial 1
+									</p>
+								</div>
+								<div class="carousel-item">
+									<p>
+										This is testimonial 2
+									</p>
+								</div>
+								<div class="carousel-item">
+									<p>
+										this is testimonial 3
+									</p>
+								</div>
+							</div>
+							<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--Block with content-->
+		<div class="content_block">
+			<div class="jumbotron jumbotron-fluid" id="portfolio">
+				<h3 class="text-center">Portfolio</h3>
+				<p class="text-center">Throughout the course of the bootcamp we had two major projects to work on.   One project is the web page you are looking at now and this project was all about front end.  I came into this bootcamp with extremely little background in anything computer related; I have come a long way!  I used HTML5, CSS, Bootstrap, and Javascript for this project.</p>
+				<hr class="my-4">
+				<p class="text-center">The second project that we had going on during the bootcamp was our capstone project that we worked on in teams.  My group and I created Family Connect.  Family Connect is a platform for families and large groups to communicate on.  What sets our site aside from others alike is a potluck and task component.  Family Connect allows families to plan potlucks, bar b que's, parties, get togethers and more.  Once an event is created, there is an automatic sign up sheet along with the event that makes it easy for everyone to sign up for what they will bring without all the hassle of going back and forth with everyone on what to bring.  The task component allows for families to assign tasks for one another with due dates.  We used MySQL, OOP, PHP, Bootstrap, CSS, SCSS, HTML5.  My group and I submitted our web app to the UNM/CNM App Competition and are hoping to come out with the W.  Click the link to check it out!</p>
+				<a class="btn btn-primary btn-lg" href="#" role="button">Family Connect</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<br>
+<br>
+
+<section id="contact">
+	<form id="Feliz's-form" action="mail-config.php" method="post">
+		<div class="form-group">
+			<h3>Send me a message and let's bring your dreams to life!</h3>
+			<label for="contactName">Name <span class="text-danger">*</span></label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"></span>
+					<i class="fa fa-user" aria-hidden="true"></i>
+				</div>
+				<input type="text" class="form-control" id="contactName" name="contactName" placeholder="Name">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="contactEmail">Email <span class="text-danger">*</span></label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"></span>
+					<i class="fa fa-envelope" aria-hidden="true"></i>
+				</div>
+				<input type="email" class="form-control" id="contactEmail" name="contactEmail" placeholder="Email">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="contactSubject">Subject</label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"></span>
+					<i class="fa fa-pencil" aria-hidden="true"></i>
+				</div>
+				<input type="text" class="form-control" id="contactSubject" name="contactSubject" placeholder="Subject">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="contactMessage">Message <span class="text-danger">*</span></label>
+			<div class="input-group">
+				<div class="input-group-prepend">
+					<span class="input-group-text"></span>
+					<i class="fa fa-comment" aria-hidden="true"></i>
+				</div>
+				<textarea class="form-control" rows="5" id="contactMessage" name="contactMessage" placeholder="Leave message here.  Can't wait to hear from you! (2000 characters max)"></textarea>
+			</div>
+		</div>
+
+		<!--recaptcha-->
+		<div class="g-recaptcha" data-sitekey="6LcLznsUAAAAAKr5Yy3iDrIqmDMEQVG6SAhemrEs"></div>
+
+		<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+		<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+	</form>
+
+	<!--empty area for form error/success output-->
+	<div class="row">
+		<div class="col-xs-12">
+			<div id="output-area"></div>
+		</div>
+	</div>
+</section>
+
+<section id="footer">
+	<footer class="container-fluid">
+		<div class="text-center">
+			<p class="small">
+				Created by Feliz Munoz
+				<br>
+				I hope you enjoyed your visit! :)
+			</p>
+		</div>
+	</footer>
+</section>
+
+</body>
+</html>
